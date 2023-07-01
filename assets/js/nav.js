5469d8bf22a2e6064dc4c37d87544ffdc43738bd
odysseum_ <118509088+ody-sseum@users.noreply.github.com>
2023-07-02 01:18:55 +0800

    Update nav.js

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,7 +1,19 @@
-const activePage = window.location.pathname;
-const navLinks = document.querySelectorAll('.nav-link').
-forEach(Link => {
-    if(Link.href.includes(`${activePage}`)){
-        Link.classList.add('active');
+// const activePage = window.location.pathname;
+// const navLinks = document.querySelectorAll('.nav-link').
+// forEach(Link => {
+//     if(Link.href.includes(`${activePage}`)){
+//         Link.classList.add('active');
+//     }
+// });
+
+
+const navLinkEls = document.querySelectorAll('.nav-link');
+const windowPathname = window.location.pathname;
+
+navLinkEls.forEach(navLinkEl => {
+    const navLinkPathname = new URL(navLinkEl.href).pathname;
+
+    if ((windowPathname === navLinkPathname) || (windowPathname === '/index.html' && navLinkPathname === '/') || (navLinkPathname === '')) {
+        navLinkEl.classList.add('active');
     }
 });
