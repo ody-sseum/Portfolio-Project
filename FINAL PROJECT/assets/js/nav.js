797f9f22803210fc0bd6b9c1bc841a4a5842becc
odysseum_ <118509088+ody-sseum@users.noreply.github.com>
2023-07-01 22:24:31 +0800

    Delete FINAL PROJECT directory

--- FINAL PROJECT/assets/js/nav.js
+++
@@ -1,7 +0,0 @@
-const activePage = window.location.pathname;
-const navLinks = document.querySelectorAll('.nav-link').
-forEach(Link => {
-    if(Link.href.includes(`${activePage}`)){
-        Link.classList.add('active');
-    }
-});
